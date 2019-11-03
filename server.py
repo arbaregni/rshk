f40a04809e2d23798819dc438e8fa68391011bb2
James Sours <soursj@PAARTHANAX.localdomain>
2019-11-03 10:04:30 -0500

    poll location data, display actual result

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ import numpy as np
 from flask import Flask, request, json, jsonify
 import os
 import urllib3, requests, json
+import traceback
+
+# access the FCC's and Census Bureau data through an API
+import census_data
+
+# the ai isn't effective at estimating injuries when the population is very low
+POP_CUTOFF = 2_000
 
 #
 # 1.  Fill in wml_credentials.
@@ -46,19 +53,28 @@ iam_token = get_iam_token()
 #
 #model_deployment_endpoint_url    = "https://us-south.ml.cloud.ibm.com/v4/deployments/1af73655-d02f-4801-864d-04b1b364c154/predictions";
 #function_deployment_endpoint_url = "https://us-south.ml.cloud.ibm.com/v4/deployments/1af73655-d02f-4801-864d-04b1b364c154/predictions";
-ENDPOINT_URL =  \
-    "https://us-south.ml.cloud.ibm.com/v4/deployments/1af73655-d02f-4801-864d-04b1b364c154/predictions";
+INJURIES_ENDPOINT_URL =  \
+    "https://us-south.ml.cloud.ibm.com/v4/deployments/1af73655-d02f-4801-864d-04b1b364c154/predictions"
+DAMAGES_ENDPOINT_URL = \
+    "https://us-south.ml.cloud.ibm.com/v4/deployments/50665275-98f7-4ee9-a458-b044a9cd2251/predictions"
 
-def createPayload( canvas_data ):
-    payload_list   = payload_arr.tolist()
-    return { "values" : payload_list }
-
-def get_samp_payload():
-    eq_primary = 7
-    population = 21_000_000
-    payload_scoring = {"input_data": [{"fields": ["EQ_PRIMARY", "POPULATION"], "values": [[eq_primary, population]] }]}
+def create_payload_injuries(magnitude, population): 
+    payload_scoring = {
+        "input_data": [{
+            "fields": ["EQ_PRIMARY", "POPULATION"],
+            "values": [[magnitude, population]]
+            }]
+    }
     return payload_scoring
 
+def create_payload_damages(magnitude, population):
+    payload_scoring = {
+        "input_data": [{
+            "fields": ["Year", "EQ_PRIMARY", "POPULATION"],
+            "values": [[2019, magnitude, population]]
+            }]
+    }
+    return payload_scoring
 
 app = Flask( __name__, static_url_path='' )
 
@@ -66,30 +82,6 @@ app = Flask( __name__, static_url_path='' )
 # When running this app on the local machine, default the port to 8000
 port = int( os.getenv( 'PORT', 8000 ) )
 
-@app.route('/dummy_test')
-def dummy_test():
-
-    import urllib3, requests, json
-
-    # NOTE: generate iam_token and retrieve ml_instance_id based on provided documentation	
-    header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + iam_token, 'ML-Instance-ID': wml_credentials['instance_id']}
-
-    # NOTE: manually define and pass the array(s) of values to be scored in the next line
-
-    payload = get_samp_payload()
-
-    response_scoring = requests.post(ENDPOINT_URL, json=payload_scoring, headers=header)
-    print("Scoring response")
-    out = json.loads(response_scoring.text)
-    print(out)
-    return response_scoring.text;
-
-
-
-    # payload = get_samp_payload()
-    # result = client.deployments.score( model_deployment_endpoint_url, payload )
-    # print( "result: " + json.dumps( result, indent=3 ) )
-    # return jsonify( result )
 
 
 @app.route('/')
@@ -99,20 +91,51 @@ def root():
 @app.route( '/send', methods=['GET', 'POST'] )
 def send():
     try:
-        print( "sending..." )
-        if ENDPOINT_URL:
-            payload = get_samp_payload()
-            result = client.deployments.score( ENDPOINT_URL, payload )
-            print('result: ' + json.dumps( result, indent=3 ))
-            prediction = result['predictions'][0]['values'][0][0]
-            print(f'prediction: {prediction}')
+        print( f"sending..." )
+        if INJURIES_ENDPOINT_URL and DAMAGES_ENDPOINT_URL:
+            # determine what type of info needs to be retrieved
+            is_injuries = request.args['injuries']
+            # parse the magnitude and location
+            magnitude = request.args['magnitude']
+            #print(f"{magnitude}")
+            lat, lng = request.args['location'].split(',')
+            lng = lng.strip()
+            # look up census data at that region
+            pop = census_data.get_population(lat, lng)
+            #print(f"population: {pop}peeps")
+
+            result = {'warn_msg': ''}
+            if pop is None:
+                warn_msg = "no population data at specified geolocation"
+                pop = 0
+            elif pop < POP_CUTOFF and pop != 0:
+                warm_msg = f"population size is small: {pop} < {POP_CUTOFF} and estimations will be imprecise"
+            
+            if pop == 0:
+                result['injuries'] = 0
+                result['damages'] = 0
+                return result
+
+            # CALCULATE EXPECTED INJURY COUNT
+            injury_payload = create_payload_injuries(magnitude, pop)
+            # query the IBM watson AutoAI
+            result['injuries'] = client.deployments.score( INJURIES_ENDPOINT_URL, injury_payload )['predictions'][0]['values'][0][0]
+            
+            # CALCULATE EXPECTED DAMAGES COST
+            damages_payload = create_payload_damages(magnitude, pop)
+            # query the IBM watson AutoAI
+            result['damages'] = client.deployments.score( DAMAGES_ENDPOINT_URL, damages_payload )['predictions'][0]['values'][0][0]
+            
+            print(result)
+            # return the result
             return jsonify( result )
         else:
             err = "endpoint URL not set in 'server.py'"
             print( "\n\nError:\n" + err )
             return jsonify( { "error" : err } )
     except Exception as e:
-        print( "\n\nError:\n" + str( e ) )
+        print( "\n\nError:\n" + traceback.format_exc() )
+        print( "")
         return jsonify( { "error" : str( e ) } )
 
 if __name__ == '__main__':
